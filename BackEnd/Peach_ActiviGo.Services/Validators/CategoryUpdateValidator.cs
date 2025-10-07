3ee84ce7d4c53823b1ec9e43ca51dfe41d52675d
JosefineNorden <iorbanderas@gmail.com>
2025-10-07 11:16:52 +0200

    Added validators for category Dtos

---
+++ BackEnd/Peach_ActiviGo.Services/Validators/CategoryUpdateValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Peach_ActiviGo.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peach_ActiviGo.Services.Validators
+{
+    public class CategoryUpdateValidator : AbstractValidator<CategoryUpdateDto>
+    {
+        public CategoryUpdateValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MinimumLength(2).MaximumLength(100);
+            RuleFor(x => x.Description).MaximumLength(500);
+        }
+    }
+}
