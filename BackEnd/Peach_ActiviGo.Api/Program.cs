3655eab568e7d928ed05ee10d25191a5a476d142
Kim Andersson <Kimpabooy@live.com>
2025-10-07 10:43:36 +0200

    Update Swagger API title to "Peach_ActiviGo API"

--- BackEnd/Peach_ActiviGo.Api/Program.cs
+++ BackEnd/Peach_ActiviGo.Api/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new() { Title = "TooliRent API", Version = "v1" });
+    c.SwaggerDoc("v1", new() { Title = "Peach_ActiviGo API", Version = "v1" });
 
     var jwtSecurityScheme = new OpenApiSecurityScheme
     {
