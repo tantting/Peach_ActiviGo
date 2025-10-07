0442a229302fa55db2152c4a9f943327c5096f7f
Kim Andersson <Kimpabooy@live.com>
2025-10-07 10:19:40 +0200

    Add AuthenticationService with interface implementation
    Added the `AuthenticationService` class in the `Peach_ActiviGo.Services.Services` namespace, implementing the `IAuthenticationService` interface.
    
    Included two methods:
    - `LoginAsync(ReadLoginDto dto)`: Placeholder for login functionality.
    - `RegisterUserAsync(CreateUserDto dto)`: Placeholder for user registration functionality.
    
    - Added necessary `using` directives for DTOs and interface dependencies.

---
+++ BackEnd/Peach_ActiviGo.Services/Services/AuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using Peach_ActiviGo.Services.DTOs.AuthDtos;
+using Peach_ActiviGo.Services.Interface;
+
+namespace Peach_ActiviGo.Services.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        Task<ReadLoginResponseDto?> IAuthenticationService.LoginAsync(ReadLoginDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IAuthenticationService.RegisterUserAsync(CreateUserDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
