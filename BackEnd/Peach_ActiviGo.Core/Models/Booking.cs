0c4bc39d5546327cbc72bfc732e89701fdec6c24
tantting <jennyann.hayward82@gmail.com>
2025-10-07 13:26:57 +0200

    Changes to models after meeting Petter

--- BackEnd/Peach_ActiviGo.Core/Models/Booking.cs
+++ BackEnd/Peach_ActiviGo.Core/Models/Booking.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace Peach_ActiviGo.Core.Models
 {
     public class Booking : BaseEntity
     {
-        public IdentityUser Customer { get; set; }
+        public string CustomerId { get; set; }           
+        public IdentityUser Customer { get; set; }      
+
         public DateTime BookingDate { get; set; } = DateTime.Now;
         public int ActivitySlotId { get; set; }
-        
-        public ActivitySlot ActivitySlot { get; set; }
+        public ActivitySlot ActivitySlot { get; set; }  
     }
 }
