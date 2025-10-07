3585240dfa87c09408ecaee768823ef90ff59473
tantting <jennyann.hayward82@gmail.com>
2025-10-07 09:32:36 +0200

    Changes to models.

--- BackEnd/Peach_ActiviGo.Core/Models/Category.cs
+++ BackEnd/Peach_ActiviGo.Core/Models/Category.cs
@@ -1,8 +1,7 @@
 ﻿namespace Peach_ActiviGo.Core.Models
 {
-    public class Category
+    public class Category : BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
 
