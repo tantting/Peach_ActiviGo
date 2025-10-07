6c4464c99cb9c2a8e2b94e8e840e2f2dd7889111
Kim Andersson <Kimpabooy@live.com>
2025-10-07 10:25:11 +0200

    Add AuthenticationController for user auth operations
    - Added a new `AuthenticationController` to handle login and
    account creation functionality. Integrated `IAuthenticationService`
    for authentication logic. Marked the controller with `[Authorize]`
    to enforce authentication. Added `Login` and `CreateUser` endpoints
    to support user authentication and registration workflows.

---
+++ BackEnd/Peach_ActiviGo.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Peach_ActiviGo.Services.DTOs.AuthDtos;
+using Peach_ActiviGo.Services.Interface;
+
+namespace Peach_ActiviGo.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authService;
+
+        public AuthenticationController(IAuthenticationService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Login")]
+        public async Task<ReadLoginResponseDto?> Login([FromBody] ReadLoginDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+
+            return result;
+        }
+
+        [HttpPut("CreateAccount")]
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserDto dto)
+        {
+            await _authService.RegisterUserAsync(dto);
+            return Ok("Account Created");
+        }
+    }
+}
