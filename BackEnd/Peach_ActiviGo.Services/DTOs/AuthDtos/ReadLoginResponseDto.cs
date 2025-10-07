ed08f1a7d81362890fe59b22e95cf2813ffcf75f
Kim Andersson <Kimpabooy@live.com>
2025-10-07 10:23:36 +0200

    Add DTO classes for user creation and authentication
    - Added `CreateUserDto`, `ReadLoginDto`, and `ReadLoginResponseDto`
    classes in the `Peach_ActiviGo.Services.DTOs.AuthDtos` namespace
    to handle user creation, login requests, and login responses.
    
    - Updated `Peach_ActiviGo.Services.csproj` to remove explicit folder
    inclusions for `DTOs`, `Interface`, and `Services` directories.

---
+++ BackEnd/Peach_ActiviGo.Services/DTOs/AuthDtos/ReadLoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Peach_ActiviGo.Services.DTOs.AuthDtos
+{
+    public class ReadLoginResponseDto
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
