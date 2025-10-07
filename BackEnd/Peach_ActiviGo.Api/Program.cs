9417c2b28e49b0d978ee39bb507c3f84c9ee41a8
Kim Andersson <Kimpabooy@live.com>
2025-10-07 13:36:46 +0200

    changed using

--- BackEnd/Peach_ActiviGo.Api/Program.cs
+++ BackEnd/Peach_ActiviGo.Api/Program.cs
@@ -1,7 +1,7 @@
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Peach_ActiviGo.Infrastructure.Data;
@@ -12,6 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddSwaggerGen(c =>
 {
@@ -33,6 +36,10 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddCors(opt => opt.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 
 //--- Jwt Authentication ---
@@ -69,14 +76,13 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     // --- Kör migrationer ---
-    //var dbContext = services.GetRequiredService<Peach_ActiviGoDbContext>();
+    var dbContext = services.GetRequiredService<AppDbContext>();
     //dbContext.Database.Migrate();
 
     // --- Seed Identity users & roles  ---
     var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
     var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
     await IdentitySeed.InitializeAsync(userManager, roleManager);
-    //object identitySeedResult = await IdentitySeed.InitializeAsync(userManager, roleManager);
 }
 
 // Configure the HTTP request pipeline.
