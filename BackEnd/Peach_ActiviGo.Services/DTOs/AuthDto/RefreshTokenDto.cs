6bed92c04a83df69a836dbc482d2ec508484f10d
Kim Andersson <Kimpabooy@live.com>
2025-10-08 14:42:27 +0200

    Add RefreshTokenDto class for authentication handling

---
+++ BackEnd/Peach_ActiviGo.Services/DTOs/AuthDto/RefreshTokenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Peach_ActiviGo.Services.DTOs.AuthDto
+{
+    public class RefreshTokenDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string? Token { get; set; } = string.Empty;
+        //public string? RefreshToken { get; set; } = string.Empty;
+    }
+}
