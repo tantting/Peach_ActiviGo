fb4e02351d2545cd2f7de32f628cedfba1790680
Kim Andersson <Kimpabooy@live.com>
2025-10-08 11:37:18 +0200

    Added DeleteUserDto class for user deletion data

---
+++ BackEnd/Peach_ActiviGo.Services/DTOs/AuthDto/DeleteUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Peach_ActiviGo.Services.DTOs.AuthDto
+{
+    public class DeleteUserDto
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
