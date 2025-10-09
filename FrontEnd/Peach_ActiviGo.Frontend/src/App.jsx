3ce221666e2aaa610bc58b5348f98825178f46e2
Kim Andersson <Kimpabooy@live.com>
2025-10-09 16:39:28 +0200

    Refactor App component structure and remove test code for cleaner routing setup

--- FrontEnd/Peach_ActiviGo.Frontend/src/App.jsx
+++ FrontEnd/Peach_ActiviGo.Frontend/src/App.jsx
@@ -1,13 +1,26 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar.jsx'
+import Aktiviteter from './Pages/Aktiviteter.jsx'
+import Kategorier from './Pages/Kategorier.jsx'
+import Kontakt from './Pages/Kontakt.jsx'
+import Login from './Pages/Login.jsx'
+import Registrera from './Pages/Registrera.jsx'
 // CSS
 import './Styles/App.css'
+import './Styles/Navbar.css'
 
 function App() {
-
   return (
-    <>
-      test
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Aktiviteter />} />
+        <Route path="/about" element={<Kategorier />} />
+        <Route path="/contact" element={<Kontakt />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Registrera />} />
+      </Routes>
+    </Router>
   )
 }
 
