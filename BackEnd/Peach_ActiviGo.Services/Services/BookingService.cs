1dd5c572347eb6db14dee45af3612457d2e11f0f
JosefineNorden <iorbanderas@gmail.com>
2025-10-11 18:03:26 +0200

    Fix IBookingService

--- BackEnd/Peach_ActiviGo.Services/Services/BookingService.cs
+++ BackEnd/Peach_ActiviGo.Services/Services/BookingService.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using Peach_ActiviGo.Core.Interface;
 using Peach_ActiviGo.Core.Models;
+using Peach_ActiviGo.Infrastructure.Data;
+using Peach_ActiviGo.Services.DTOs.BookingDtos;
 using Peach_ActiviGo.Services.Interface;
 
 namespace Peach_ActiviGo.Services.Services;
@@ -9,26 +11,30 @@ public class BookingService : IBookingService
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
-    
-    public BookingService(IUnitOfWork unitOfWork, IMapper mapper)
+    private readonly AppDbContext _context;
+
+    public BookingService(IUnitOfWork unitOfWork, IMapper mapper, AppDbContext context)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _context = context;
     }
 
-    public Task<IEnumerable<Booking>> GetAllBookingsAsync(CancellationToken ct)
+    public async Task<IEnumerable<BookingDto>> GetAllBookingsAsync(CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var entities = await _unitOfWork.Bookings.GetAllBookingsAsync(ct);
+        return _mapper.Map<IEnumerable<BookingDto>>(entities);
     }
 
-    public Task<Booking> GetBookingByIdAsync(int id, CancellationToken ct)
+    public async Task<BookingDto?> GetBookingByIdAsync(int id, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var entity = await _unitOfWork.Bookings.GetBookingByIdAsync(id, ct);
+        return entity is null ? null : _mapper.Map<BookingDto>(entity);
     }
 
-    public Task AddBookingAsync(Booking booking, CancellationToken ct)
+    public Task AddBookingAsync(BookingDto booking, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        
     }
 
     public Task UpdateBookingAsync(Booking booking, CancellationToken ct)
