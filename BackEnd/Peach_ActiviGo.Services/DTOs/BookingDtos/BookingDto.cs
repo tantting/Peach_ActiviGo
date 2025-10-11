e80a092acad70ca935fbc0790d5423ed66c43ec2
JosefineNorden <iorbanderas@gmail.com>
2025-10-11 17:36:07 +0200

    Add BookingDto

---
+++ BackEnd/Peach_ActiviGo.Services/DTOs/BookingDtos/BookingDto.cs
@@ -0,0 +1,23 @@
+﻿using Peach_ActiviGo.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peach_ActiviGo.Services.DTOs.BookingDtos
+{
+    public class BookingDto
+    {
+        public int Id { get; set; }
+        public string CustomerId { get; set; }
+        public int ActivitySlotId { get; set; }
+        public DateTime BookingDate { get; set; }
+
+        public BookingStatus Status { get; set; }
+        public DateTime? CancelledAt { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsUpcoming { get; set; }
+    }
+}
