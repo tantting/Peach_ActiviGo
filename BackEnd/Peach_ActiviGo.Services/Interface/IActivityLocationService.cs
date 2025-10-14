19abbcf1807a3f002453b4fb34ab40e26a617119
Kim Andersson <Kimpabooy@live.com>
2025-10-14 11:47:39 +0200

    Add FilterActivityLocationsAsync to IActivityLocationService

--- BackEnd/Peach_ActiviGo.Services/Interface/IActivityLocationService.cs
+++ BackEnd/Peach_ActiviGo.Services/Interface/IActivityLocationService.cs
@@ -6,5 +6,6 @@ namespace Peach_ActiviGo.Services.Interface
     {
         Task<bool> UpdateActivityLocationStatusAsync(UpdateActivityLocationDto dto, CancellationToken ct);
         Task<IEnumerable<ReadActivityLocationDto>> GetAllActivityLocationsAsync(CancellationToken ct);
+        Task<IEnumerable<ReadActivityLocationDto>> FilterActivityLocationsAsync(ActivityLocationFilterDto filter, CancellationToken ct);
     }
 }
