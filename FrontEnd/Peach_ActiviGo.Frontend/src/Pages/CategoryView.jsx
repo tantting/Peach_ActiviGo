d6e19ea5e2612627a4074c3401f9d643a1e15081
Kim Andersson <Kimpabooy@live.com>
2025-10-15 14:09:51 +0200

    Renamed files to English+views for activities, categories, contact, login, and registration

--- FrontEnd/Peach_ActiviGo.Frontend/src/Pages/CategoryView.jsx
+++ FrontEnd/Peach_ActiviGo.Frontend/src/Pages/CategoryView.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Kategorier() {
+export default function CategoryView() {
   return (
     <div className="page-container">
       <h1>Kategorier</h1>
