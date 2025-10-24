6885abe027be2f63e7639654bd5b43ddbafb6145
Kim Andersson <Kimpabooy@live.com>
2025-10-24 16:25:57 +0200

    another save

--- BackEnd/Peach_ActiviGo.Api/MiddleWare/GlobalExceptionMiddleware.cs
+++ BackEnd/Peach_ActiviGo.Api/MiddleWare/GlobalExceptionMiddleware.cs
@@ -1,7 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Net;
+﻿using System.Net;
 using System.Text.Json;
-using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
