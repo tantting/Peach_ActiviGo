21a088b13759e1f5baa50a0991c2fcea2174dc6d
tantting <jennyann.hayward82@gmail.com>
2025-10-27 19:42:18 +0100

    Add validation for NumberOfParticipants in CreateBookingDtoValidator

--- BackEnd/Peach_ActiviGo.Services/Validators/CreateBookingDtoValidator.cs
+++ BackEnd/Peach_ActiviGo.Services/Validators/CreateBookingDtoValidator.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using Peach_ActiviGo.Services.DTOs.BookingDtos;
+using Peach_ActiviGo.Services.Interface;
 
 namespace Peach_ActiviGo.Services.Validators;
 
@@ -8,5 +9,6 @@ public class CreateBookingDtoValidator : AbstractValidator<BookingCreateDto>
     public CreateBookingDtoValidator()
     {
         RuleFor(x => x.ActivitySlotId).GreaterThan(0).WithMessage("ActivitySlotId must be greater than 0.");
+        RuleFor(x => x.NumberOfParticipants).GreaterThan(0).WithMessage("NumberOfParticipants must be greater than 0."); 
     }
 }
